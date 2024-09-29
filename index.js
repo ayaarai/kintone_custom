6a323587ecedd45f92a29480474c7c8c653e3378
ayaarai <ayaarai264@gmail.com>
2024-09-29 15:45:07 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//kintone API カスタマイズ
+//新規登録
+kintone.events.on('app.record.create.show', (event) => {
+    // イベントで行いたい処理を書く
+});
+//編集時登録
+kintone.events.on('app.record.edit.show', (event) => {
+    // イベントで行いたい処理を書く
+});
